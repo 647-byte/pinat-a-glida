df0a965e66a3fa58b02c3d93abf39f0ed9115361
r <r@2>
2026-08-20 12:41:13 +0300

    add flexible Product schema with options

---
+++ models/products.model.js
@@ -0,0 +1,32 @@
+import { model, Schema } from "mongoose";
+const sizeSchema = new Schema(
+    {
+        name: { type: String, required: true, trim: true },
+        price: { type: Number, required: true, min: 0 },
+        maxChoices: { type: Number, min: 1 }
+    },
+    { _id: false });
+const optionsSchema = new Schema(
+    {
+        title: { type: String, required: true, trim: true },
+        isMust: { type: Boolean, default: true },
+        maxChoices: { type: Number, required: true, min: 1 },
+        pricePer: { type: Number, default: 0, min: 0 },
+        choices: [{ type: String, trim: true }]
+    },
+    { _id: false });
+const productSchema = new Schema(
+    {
+        name: { type: String, unique: true, required: true, trim: true },
+        price: { type: Number, required: true, default: 0, min: 0 },
+        description: { type: String, trim: true },
+        category: { type: String, required: true, trim: true },
+        imageUrl: { type: String, required: true, trim: true },
+        isAvailable: { type: Boolean, default: true },
+        size: { type: [sizeSchema] },
+        options: { type: [optionsSchema] }
+    },
+    { timestamps: true }
+)
+const Product = model('Product', productSchema);
+export default Product;
